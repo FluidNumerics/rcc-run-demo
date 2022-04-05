cc77a51568733ee91878cff1fe0f92ec9ec7b636
Joe Schoonover <joe@fluidnumerics.com>
2022-04-05 15:26:01 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# RCC-Run (Demo)
+Copyright 2022  Fluid Numerics LLC
+
+This repository contains code used to teach how to get started with continuous benchmarking by using [Google Cloud Build](https://cloud.google.com/build) and [RCC-Run](https://fluid-run.readthedocs.io/en/latest/)
+
+## Documentation
+
+* [Codelab Tutorial : Get Started with RCC-Run]()
+* [RCC-Run Documentation](https://fluid-run.readthedocs.io/en/latest/)
+* [Research Computing Cloud (RCC) Documentation](https://research-computing-cluster.readthedocs.io/en/latest/)
---
+++ ci/tf/main.tf
@@ -0,0 +1,43 @@
+
+/*
+terraform {
+  backend "gcs" {
+    bucket  = "YOUR GCS BUCKET"
+    prefix  = "rcc-run"
+  }
+}
+*/
+
+module "rcc-run" {
+  source = "github.com/FluidNumerics/rcc-run//tf/"
+  bq_location = var.bq_location
+  project = var.project
+  subnet_cidr = var.subnet_cidr
+  whitelist_ssh_ips = var.whitelist_ssh_ips
+}
+
+locals {
+  bld_description = {for bld in var.builds : bld.name => bld.description}
+  bld_branch = {for bld in var.builds : bld.name => bld.branch}
+  bld_image = {for bld in var.builds : bld.name => bld.image}
+  bld_zone = {for bld in var.builds : bld.name => bld.zone}
+}
+
+resource "google_cloudbuild_trigger" "builds" {
+  for_each = local.bld_description
+  name = each.key
+  project = var.project
+  description = each.value
+  github {
+    owner = var.github_owner
+    name = var.github_repo
+    push {
+      branch = local.bld_branch[each.key]
+    }
+  }
+  substitutions = {
+  _ZONE = local.bld_zone[each.key]
+  _IMAGE = local.bld_gce_image[each.key]
+  }
+  filename = var.cloudbuild_path
+}
